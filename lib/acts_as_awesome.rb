cb8b964d64b7ce9334f58e4ecc33b40c1360f383
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2010-10-31 20:59:15 -0400

    t

--- lib/acts_as_awesome.rb
+++ lib/acts_as_awesome.rb
@@ -1,5 +1,6 @@
 require 'active_support/core_ext'
 p "loading main"
+require File.join(File.dirname(__FILE__), "acts_as_awesome/railtie.rb")
 
 module ActsAsAwesome
   autoload :Hook, File.join(File.dirname(__FILE__), "acts_as_awesome/hook")
